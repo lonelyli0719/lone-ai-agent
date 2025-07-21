0831b5fade126ba35baed2512d97dc76cd6d41dc
lonelyli0719 <794156416@qq.com>
2025-07-21 23:16:21 +0800

    AI智能体 ai服务化

--- src/main/java/com/lone/loneaiagent/app/LoveApp.java
+++ src/main/java/com/lone/loneaiagent/app/LoveApp.java
@@ -17,6 +17,7 @@ import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -266,5 +267,21 @@ public class LoveApp {
         return content;
     }
 
+
+    /**
+     * sse流式传输
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public Flux<String> doChatByStream(String message, String chatId) {
+        return chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .stream()
+                .content();
+    }
 
 }
---
+++ src/main/java/com/lone/loneaiagent/controller/AiController.java
@@ -0,0 +1,106 @@
+package com.lone.loneaiagent.controller;
+
+import com.lone.loneaiagent.agent.LoneManus;
+import com.lone.loneaiagent.app.LoveApp;
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/ai")
+public class AiController {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Resource
+    private ToolCallback[] allTools;
+
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+    /**
+     * 同步调用AI恋爱大师应用
+     * @param message
+     * @param chatId
+     * @return
+     */
+    @GetMapping("/love_app/chat/sync")
+    public String doChatWithLoveAppSync(String message, String chatId) {
+        return loveApp.doChat(message, chatId);
+    }
+
+
+    @GetMapping(value = "/love_app/chat/server_sent_event")
+    public Flux<ServerSentEvent<String>> doChatWithLoveAppServeSentEvent(String message, String chatId) {
+        return loveApp.doChatByStream(message, chatId)
+                .map(chunk -> ServerSentEvent.<String>builder()
+                        .data(chunk)
+                        .build());
+    }
+
+    /**
+     * MediaType.TEXT_EVENT_STREAM_VALUE：http的响应头中的Content-Type为text/event-stream
+     * @param message
+     * @param chatId
+     * @return
+     */
+    @GetMapping(value = "/love_app/chat/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> doChatWithLoveAppSSE(String message, String chatId) {
+        return loveApp.doChatByStream(message, chatId);
+    }
+
+    /**
+     * SseEmitter,实现sse推流
+     * sse，服务端单向发送，客户端可以实时接收服务端的数据
+     * @param message
+     * @param chatId
+     * @return
+     */
+    @GetMapping("/love_app/chat/sse/emitter")
+    public SseEmitter doChatWithLoveAppSseEmitter(String message, String chatId) {
+        // 创建一个超时时间较长的 SseEmitter
+        SseEmitter emitter = new SseEmitter(180000L); // 3分钟超时
+        // 获取 Flux 数据流并直接订阅
+        loveApp.doChatByStream(message, chatId)
+                .subscribe(
+                        // 处理每条消息
+                        chunk -> {
+                            try {
+                                emitter.send(chunk);
+                            } catch (IOException e) {
+                                emitter.completeWithError(e);
+                            }
+                        },
+                        // 处理错误
+                        emitter::completeWithError,
+                        // 处理完成
+                        emitter::complete
+                );
+        // 返回emitter
+        return emitter;
+    }
+
+
+    /**
+     * 流式调用 Manus 超级智能体
+     *
+     * @param message
+     * @return
+     */
+    @GetMapping("/manus/chat")
+    public SseEmitter doChatWithManus(String message) {
+        LoneManus yuManus = new LoneManus(allTools, dashscopeChatModel);
+        return yuManus.runStream(message);
+    }
+
+}
